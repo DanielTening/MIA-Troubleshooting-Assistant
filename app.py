fd6169cbd5e38488a1f528f0049b84378a0896a6
DanielTening <awah.daniel@yahoo.com>
2026-04-10 10:04:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -547,10 +547,12 @@ lang = st.sidebar.selectbox("Language / Langue", ["English", "Français"])
 T = TEXT[lang]
 
 users_db = load_users()
+
 st.title(T["app_title"])
 st.caption(T["app_subtitle"])
+
 if not st.session_state.authenticated:
-    st.subheader(T["login_title"])
+    st.title(T["login_title"])
     col1, col2 = st.columns([1, 2])
 
     with col1:
