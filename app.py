8190eef66b974e075fce09fa630ff122ca3ed2b5
DanielTening <awah.daniel@yahoo.com>
2026-04-10 09:58:53 +0100

    Update app.py

--- app.py
+++ app.py
@@ -550,7 +550,7 @@ users_db = load_users()
 st.title(T["app_title"])
 st.caption(T["app_subtitle"])
 if not st.session_state.authenticated:
-    st.title(T["login_title"])
+    st.subheader(T["login_title"])
     col1, col2 = st.columns([1, 2])
 
     with col1:
